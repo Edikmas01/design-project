113f09975fbcfb52c39f52ef48b021041aa9fc45
Edikmas01 <edikmaslovskiy@gmail.com>
2024-08-15 18:40:29 +0200

    creating the Services page and Modal window

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,140 @@
+import { useState } from "react";
 import "./Modal.scss";
+import { services } from "../../../public/api/services";
 
-export const Modal = () => {
-  return <></>;
+export const Modal = ({ onClose, isOpen }) => {
+  const [selectedService, setSelectedService] = useState("");
+
+  const handleServiceChange = (e) => {
+    setSelectedService(e.target.value);
+  };
+
+  return (
+    <div className={`modal ${isOpen ? "" : "is-hidden"}`}>
+      <button
+        type="button"
+        className="modal-btn"
+        data-modal-close
+        onClick={onClose}
+      >
+        {/* <svg className="beckdrop-icon" width="11" height="11">
+          <use href="./photo/symbol-defs.svg#icon-exit"></use>
+        </svg> */}
+        x
+      </button>
+      <h3 className="modal-title">Залиште свої дані, ми вам передзвонимо</h3>
+      <form className="modal-form">
+        <div className="modal-div">
+          <label htmlFor="user-name" className="form-label">
+            Ім'я
+          </label>
+          <div className="imput-wrap">
+            <input
+              type="text"
+              name="user-name"
+              className="modal-input"
+              id="user-name"
+            />
+            <svg className="input-icon" width="18" height="18">
+              <use href="./photo/symbol-defs.svg#icon-name"></use>
+            </svg>
+          </div>
+        </div>
+        <div className="modal-div">
+          <label htmlFor="user-tel" className="form-label">
+            Телефон
+          </label>
+          <div className="imput-wrap">
+            <input
+              type="tel"
+              name="user-tel"
+              className="modal-input"
+              id="user-tel"
+            />
+            <svg className="input-icon" width="18" height="18">
+              <use href="./photo/symbol-defs.svg#icon-email"></use>
+            </svg>
+          </div>
+        </div>
+        <div className="modal-div">
+          <label htmlFor="user-email" className="form-label">
+            Пошта
+          </label>
+          <div className="imput-wrap">
+            <input
+              type="email"
+              name="user-email"
+              className="modal-input"
+              id="user-email"
+            />
+            <svg className="input-icon" width="18" height="18">
+              <use href="./photo/symbol-defs.svg#icon-tel"></use>
+            </svg>
+          </div>
+        </div>
+        <div className="modal-div">
+          <label htmlFor="service-select" className="form-label">
+            Выбрать услугу
+          </label>
+          <select
+            id="service-select"
+            className="modal-input"
+            value={selectedService}
+            onChange={handleServiceChange}
+          >
+            <option value="">Выберите услугу</option>
+            {services.map((service) => (
+              <option key={service.id} value={service.title}>
+                {service.title}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="modal-div">
+          <label htmlFor="user-text" className="form-label">
+            Коментар
+          </label>
+          <textarea
+            name="user-text"
+            id="user-text"
+            placeholder="Введіть текст"
+            className="modal-text"
+          ></textarea>
+        </div>
+
+        <div className="chec">
+          <input
+            type="checkbox"
+            name="agree"
+            id="agree"
+            className="modal-chec visually-hidden"
+            value="true"
+          />
+          <label htmlFor="agree" id="agree-label" className="chec-text">
+            Погоджуюся з розсилкою та приймаю &nbsp;
+            <a href="" className="chec-link">
+              Умови договору
+            </a>
+          </label>
+        </div>
+
+        <button type="submit" className="form-btn">
+          Відправити
+        </button>
+      </form>
+    </div>
+  );
 };
+
+//  <span className="chec-icon-wrap">
+//    <svg className="chec-icon" width="11" height="8">
+//      <use href="./photo/symbol-defs.svg#icon-checkmark"></use>
+//    </svg>
+//  </span>;
+
+// const [isOpen, setIsOpen] = useState(false);
+
+// const toggleModal = () => {
+//   setIsOpen(!isOpen);
+// };
+// className={`beckdrop ${isOpen ? "" : "is-hidden"}`} data-modal
--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -1,5 +1,88 @@
+import { useState } from "react";
 import "./ServicesPage.scss";
+import { NavLink } from "react-router-dom";
+import { services } from "../../../public/api/services";
+import { Modal } from "../../components/Modal/Modal";
 
 export const ServicesPage = () => {
-  return <><h1>56</h1></>;
+  const [selectedService, setSelectedService] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleServiceClick = (service) => {
+    setSelectedService(service);
+    console.log("Selected service:", service);
+  };
+
+  const handleOrderService = () => {
+    console.log("Order service button clicked");
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    console.log("Modal closed");
+    setModalOpen(false);
+  };
+
+  return (
+    <section className="service-section ">
+      <div className="option">
+        <div className="beckgroundImg"></div>
+        <div className="oooo">
+          <h1 className="option-title">Services</h1>
+          <ul className="option-list">
+            {services.map((service) => (
+              <li className="option-item" key={service.id}>
+                <NavLink
+                  className="option-item-nav"
+                  onClick={() => handleServiceClick(service)}
+                >
+                  {service.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <button
+            type="button"
+            className="option-bnt"
+            onClick={() => handleOrderService()}
+          >
+            order service
+          </button>
+        </div>
+      </div>
+      <h1 className="description-title">
+        {selectedService ? selectedService.title : "Select a service"}
+      </h1>
+      <div className="description-and-price site-container">
+        <div className="description">
+          <p className="description-text">
+            {selectedService
+              ? selectedService.description
+              : "Click on a service to see its description"}
+          </p>
+          {selectedService && (
+            <h3 className="price">Price: {selectedService.price}м²</h3>
+          )}
+        </div>
+        {selectedService && (
+          <div className="includes">
+            <h3 className="includes-title">includes</h3>
+            <ul className="includes-list">
+              {selectedService.plen.map((item, index) => (
+                <li key={index} className="includes-item">
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      {selectedService && (
+        <div className="image-container site-container">
+          <img className="img-services" src={selectedService.image} alt="" />
+        </div>
+      )}
+      {modalOpen && <Modal onClose={handleCloseModal} isOpen={modalOpen} />}
+    </section>
+  );
 };
