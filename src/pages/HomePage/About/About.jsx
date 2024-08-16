434bf375ecb8509bac126816a8c4d1ff8ce60a3f
Edikmas01 <edikmaslovskiy@gmail.com>
2024-08-16 12:38:42 +0200

    Change the About section. Adding an open form button to the Hero section

--- src/pages/HomePage/About/About.jsx
+++ src/pages/HomePage/About/About.jsx
@@ -3,28 +3,31 @@ import "./About.scss";
 
 export const About = () => {
   return (
-    <section className="about-container site-container">
-      <h2 className="interior-title">About me</h2>
-      <div className="interior-spaces">
-        <div className="photo-container">
-          <img src="/photo/photo3.png" alt="" className="photo-image" />
+    <>
+      <section className="about-section ">
+        <div className="about-content site-container">
+          <div className="about-image">
+            <img src="/public/photo/about.jpg" alt="About me" />
+          </div>
+          <div className="about-text">
+            <h1>About me</h1>
+            <p>
+              We create bespoke and beautiful interior spaces that reflect your
+              personality We create bespoke and beautiful interior spaces that
+              reflect your personalityWe create bespoke and beautiful interior
+              spaces that reflect your personalityWe create bespoke and
+              beautiful interior spaces that reflect your personalityWe create
+              bespoke and beautiful interior spaces that reflect your
+              personalityWe create bespoke and beautiful interior spaces that
+              reflect your personalityWe create bespoke and beautiful interior
+              spaces that reflect your personalityWe create bespoke and
+              beautiful interior spaces that reflect your personalityWe create
+              bespoke and beautiful interior
+            </p>
+          </div>
         </div>
-        <div className="about-description">
-          <p className="interior-spaces-description">
-            We create bespoke and beautiful interior spaces that reflect your
-            personality We create bespoke and beautiful interior spaces that
-            reflect your personalityWe create bespoke and beautiful interior
-            spaces that reflect your personalityWe create bespoke and beautiful
-            interior spaces that reflect your personalityWe create bespoke and
-            beautiful interior spaces that reflect your personalityWe create
-            bespoke and beautiful interior spaces that reflect your
-            personalityWe create bespoke and beautiful interior spaces that
-            reflect your personalityWe create bespoke and beautiful interior
-            spaces that reflect your personalityWe create bespoke and beautiful
-            interior
-          </p>
-        </div>
-      </div>
-    </section>
+        <div className="about-line"></div>
+      </section>
+    </>
   );
 };
--- src/pages/HomePage/Hero/Hero.jsx
+++ src/pages/HomePage/Hero/Hero.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./Hero.scss";
+import { Modal } from "../../../components/Modal/Modal";
 
 export const Hero = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+    const handleOrderService = () => {
+      console.log("Order service button clicked");
+      setModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+      console.log("Modal closed");
+      setModalOpen(false);
+    };
 
   return (
     <section className="hero-container">
       <div className="studio-background-image"></div>
-      <div className="site-container">
-        <h1 className="interior-design-title site-container">
+      <div className="hero-content site-container">
+        <h1 className="interior-design-title">
           Welcome to Studio
           <br />
           InteriorDesign
@@ -15,7 +28,15 @@ export const Hero = () => {
           We create bespoke and beautiful interior spaces that reflect your
           personality
         </p>
+        <button
+          type="button"
+          className="option-bnt"
+          onClick={() => handleOrderService()}
+        >
+          order service
+        </button>
       </div>
+      {modalOpen && <Modal onClose={handleCloseModal} isOpen={modalOpen} />}
     </section>
   );
 };
