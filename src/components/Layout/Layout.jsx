c3128e33bea20a87323e3ce2c5c08c7b497bc108
Edikmas01 <edikmaslovskiy@gmail.com>
2024-09-03 18:00:02 +0200

    Manual deploy

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,15 @@
-import { Header } from "../Header/Header";
-import { Footer } from "../Footer/Footer";
+// import { Header } from "../Header/Header";
+// import { Footer } from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main>
         <Outlet />
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
